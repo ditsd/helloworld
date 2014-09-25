e3e66885b40091141ba7f228f73f8449f1b886d6
ditsd <difeng@dis-b.home>
2014-09-25 10:32:39 -0400

    test run 4

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld { 
    public static void main(String[] args) { 
-      System.out.println("Hello, World test run 3");
+      System.out.println("Hello, World test run 4");
    }
 }
